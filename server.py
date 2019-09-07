5c25ec8d5e6fa9741323cb62e742fac5258ee25a
Ralph Gasser <ralph.gasser@unibas.ch>
2019-09-07 17:18:14 +0200

    Ajusted routes.

--- server.py
+++ server.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_restful import Api
 
 from services.PostcardAudio import PostcardAudio
+from services.PostcardImageInfo import PostcardImageInfo
 from services.PostcardRandom import PostcardRandom
 from services.Generate import Generate
 from services.History import History
 from services.PostcardList import PostcardList
-from services.PostcardAccess import PostcardAccess
+from services.PostcardImage import PostcardImage
 from services.Snapshot import Snapshot
 
 app = Flask(__name__)
@@ -14,8 +15,9 @@ api = Api(app)
 
 api.add_resource(PostcardList, '/postcard/list') # Route_1: Postcard list
 api.add_resource(PostcardRandom, '/postcard/random') # Route_2: Random postcard
-api.add_resource(PostcardAccess, '/postcard/image/<postcard_id>') # Route_3: Access postcard image
-api.add_resource(PostcardAudio, '/postcard/audio/<postcard_id>') # Route_4: Access postcard ambient audio
+api.add_resource(PostcardImageInfo, '/postcard/image/info/<postcard_id>') # Route_3: Access postcard image
+api.add_resource(PostcardImage, '/postcard/image/<postcard_id>') # Route_4: Access postcard image
+api.add_resource(PostcardAudio, '/postcard/audio/<postcard_id>') # Route_5: Access postcard ambient audio
 
 api.add_resource(History, '/history/list') # Route_5: History list
 api.add_resource(Snapshot, '/history/image/<snapshot_id>') # Route_6: Access snapshot
