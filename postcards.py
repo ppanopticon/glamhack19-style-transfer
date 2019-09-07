7e3d102e38254add321abdf3010656954f4771a6
Ralph Gasser <ralph.gasser@unibas.ch>
2019-09-07 17:35:54 +0200

    Added postcard images and audio file + mapping.

---
+++ postcards.py
@@ -0,0 +1,31 @@
+POSTCARDS = {
+    "C1145_1": {"image": "C1145_1.jpg", "audio": "city.mp3", "class": "city"} ,
+    "C18_1": {"image": "C18_1.jpg", "audio": "city.mp3", "class": "city"} ,
+    "C56_1": {"image": "C56_1.jpg", "audio": "city.mp3", "class": "city"} ,
+    "C910_1": {"image": "C910_1.jpg", "audio": "city.mp3", "class": "city"} ,
+    "C919_1": {"image": "C919_1.jpg", "audio": "city.mp3", "class": "city"},
+
+    "C5071_1": {"image": "C5071_1.jpg", "audio": "countryside.mp3", "class": "countryside"},
+    "C4578_1": {"image": "C4578_1.jpg", "audio": "countryside.mp3", "class": "countryside"},
+    "C4387_1": {"image": "C4387_1.jpg", "audio": "countryside.mp3", "class": "countryside"},
+    "C1018_1": {"image": "C1018_1.jpg", "audio": "countryside.mp3", "class": "countryside"},
+    "055ph-00114": {"image": "055ph-00114.jpg", "audio": "countryside.mp3", "class": "countryside"},
+
+    "C4206_1": {"image": "C4206_1.jpg", "audio": "tourism.mp3", "class": "tourism"},
+    "C4598_1": {"image": "C4598_1.jpg", "audio": "tourism.mp3", "class": "tourism"},
+    "C4609_1": {"image": "C4609_1.jpg", "audio": "tourism.mp3", "class": "tourism"},
+    "C4618_1": {"image": "C4618_1.jpg", "audio": "tourism.mp3", "class": "tourism"},
+    "C5223_1": {"image": "C5223_1.jpg", "audio": "tourism.mp3", "class": "tourism"},
+
+    "C4350_1": {"image": "C4350_1.jpg", "audio": "vehicles.mp3", "class": "vehicles"},
+    "C4585_1": {"image": "C4585_1.jpg", "audio": "vehicles.mp3", "class": "vehicles"},
+    "C4586_1": {"image": "C4586_1.jpg", "audio": "vehicles.mp3", "class": "vehicles"},
+    "C5180_1": {"image": "C5180_1.jpg", "audio": "vehicles.mp3", "class": "vehicles"},
+    "C5233_1": {"image": "C5233_1.jpg", "audio": "vehicles.mp3", "class": "vehicles"},
+
+    "C1334_1": {"image": "C1334_1.jpg", "audio": "winter.mp3", "class": "winter"},
+    "C1335_1": {"image": "C1335_1.jpg", "audio": "winter.mp3", "class": "winter"},
+    "C1839_1": {"image": "C1839_1.jpg", "audio": "winter.mp3", "class": "winter"},
+    "C1863_1": {"image": "C1863_1.jpg", "audio": "winter.mp3", "class": "winter"},
+    "C4823_1": {"image": "C4823_1.jpg", "audio": "winter.mp3", "class": "winter"}
+}
