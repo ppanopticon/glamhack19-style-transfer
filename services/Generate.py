5f0c3426052a1a89482df9ab05dc7f0c24e5e28c
Ralph Gasser <ralph.gasser@unibas.ch>
2019-09-07 17:05:15 +0200

    Fixed recombination of images (postcard background + style transfered foreground)

--- services/Generate.py
+++ services/Generate.py
@@ -11,6 +11,10 @@ from flask import request
 from model import Model
 
 
+
+LOWER_BLUE = np.array([0, 0, 100])
+UPPER_BLUE = np.array([120, 100, 255])
+
 class Generate(Resource):
     def post(self, postcard_id):
 
@@ -19,10 +23,12 @@ class Generate(Resource):
             data = np.fromstring(request.data, np.uint8)
             style = cv2.imread(os.path.join('postcards', 'images', postcard_id + '.jpg'), cv2.IMREAD_COLOR)
             image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+            image = image[0:min(image.shape[0], style.shape[0]), 0:min(image.shape[1], style.shape[1])]
+
 
             if style is not None and image is not None:
                 # Generate filename and read file.
-                filename = str(uuid.uuid4()) + '.jpg'
+                filename = str(uuid.uuid4())
 
                 # Invoke model (new thread) and generate stuff (new thread).
                 thread = StyleTransferThread(image, style, filename)
@@ -49,24 +55,21 @@ class StyleTransferThread(threading.Thread):
 
     # Run method (executed in parallel)
     def run(self):
-
         #Instantiate model for style transfer
         model = Model(self.input, self.style)
 
         # Create mask for blue screen replacement.
-        lower_blue = np.array([0, 0, 100])
-        upper_blue = np.array([120, 100, 255])
-        mask = cv2.resize(self.input, (model.width, model.height))
-        mask = cv2.inRange(mask, lower_blue, upper_blue)
+        mask = cv2.cvtColor(self.input, cv2.COLOR_BGR2RGB)
+        mask = cv2.resize(mask, (model.width, model.height))
+        mask = cv2.inRange(mask, LOWER_BLUE, UPPER_BLUE)
 
-        # Generate style transfered image and mask it based on blue screen.
+        # Generate style transferred image and mask it based on blue screen.
         masked_image = model.run(10)
         masked_image[mask != 0] = [0, 0, 0]
 
         # Mask background image.
         background_image = cv2.resize(self.style, (model.width, model.height))
-        background_image = cv2.cvtColor(background_image, cv2.COLOR_BGR2RGB)
         background_image[mask == 0] = [0, 0, 0]
 
         # Export combined image.
-        cv2.imwrite(os.path.join('./snapshots/', self.output), background_image + masked_image)
+        cv2.imwrite(os.path.join('snapshots', self.output + '.jpg'), background_image + masked_image)
