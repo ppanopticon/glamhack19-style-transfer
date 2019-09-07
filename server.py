7dd8bf85a1844fa7a55d70ac0aa6858601b2442f
Ralph Gasser <ralph.gasser@unibas.ch>
2019-09-07 11:01:22 +0200

    Added routes to access snapshot history + snapshot

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_restful import Api
 
+from services.History import History
 from services.Postcard import Postcard
 from services.Postcards import Postcards
+from services.Snapshot import Snapshot
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Postcards, '/postcards') # Route_1: Postcard list
-api.add_resource(Postcard, '/postcard/<postcard_id>') # Route_2: Postcard
+api.add_resource(Postcard, '/postcard/<postcard_id>') # Route_2: Access postcard
+api.add_resource(History, '/history') # Route_3: History
+api.add_resource(Snapshot, '/snapshot/<postcard_id>') # Route_3: Access snapshot
 
 if __name__ == '__main__':
-     app.run(port='5002')
+     app.run(port='5002', host='0.0.0.0')
